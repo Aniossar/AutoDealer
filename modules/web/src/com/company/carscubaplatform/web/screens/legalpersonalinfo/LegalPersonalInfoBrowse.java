23fe1f51b29df9e7bb6e67d8c3d0f652c46fbb90
Aniossar <aniossar@yandex.ru>
2023-03-14 13:39:07 +0400

    added printable bumberOfRequestsForBuyer

--- modules/web/src/com/company/carscubaplatform/web/screens/legalpersonalinfo/LegalPersonalInfoBrowse.java
+++ modules/web/src/com/company/carscubaplatform/web/screens/legalpersonalinfo/LegalPersonalInfoBrowse.java
@@ -27,12 +27,17 @@ public class LegalPersonalInfoBrowse extends StandardLookup<LegalPersonalInfo> {
     @Override
     protected void initActions(InitEvent event) {
         super.initActions(event);
-        legalPersonalInfoesTable.addGeneratedColumn("generatedColumnNumberOfRequests", new Table.ColumnGenerator<LegalPersonalInfo>() {
+        legalPersonalInfoesTable.addGeneratedColumn("generatedColumnNumberOfRequests", new Table.PrintableColumnGenerator<LegalPersonalInfo, Integer>() {
             @Override
             public Component generateCell(LegalPersonalInfo entity) {
                 Label field = (Label) componentsFactory.createComponent(Label.NAME);
                 field.setValue(autoBuyingRequestService.getNumberOfRequestsForBuyer(entity));
                 return field;
+            }
+
+            @Override
+            public Integer getValue(LegalPersonalInfo item) {
+                return autoBuyingRequestService.getNumberOfRequestsForBuyer(item);
             }
         });
     }
